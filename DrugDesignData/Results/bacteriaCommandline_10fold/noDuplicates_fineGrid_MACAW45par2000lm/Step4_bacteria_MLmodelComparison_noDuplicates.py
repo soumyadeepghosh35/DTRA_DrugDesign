bab6da8ca470a5628a1f36d2c81bfb65b4be7625
soumyadeepghosh35 <soumyadeepghosh35@gmail.com>
2026-05-06 17:06:13 -0700

    Commit DrugDesignData code; ignore large data files by extension

---
+++ DrugDesignData/Results/bacteriaCommandline_10fold/noDuplicates_fineGrid_MACAW45par2000lm/Step4_bacteria_MLmodelComparison_noDuplicates.py
@@ -0,0 +1,1 @@
+/users/sghosh6/DTRA_project/MACAW/PubchemData/Step4_bacteria_MLmodelComparison_noDuplicates.py --config config_noDuplicate.yaml > noDuplicate_out.txt
---
+++ DrugDesignData/Results/virusCommandline_10fold/wDuplicate_MACAW45par4000lm/Step4_virus_MLmodelComparison.py
@@ -0,0 +1,1 @@
+/users/sghosh6/DTRA_project/MACAW/DTRA_project_notebooks/Step4_virus_MLmodelComparison.py --config config.yaml > wDuplicate_out.txt
